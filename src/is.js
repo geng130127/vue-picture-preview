87d724b45b908f90a7283a7bb96ada82a4239e72
xuyang.geng <574313000@qq.com>
2022-03-04 09:45:30 +0800

    增加判断是否为undefined

--- src/is.js
+++ src/is.js
@@ -3,3 +3,6 @@ const toString = Object.prototype.toString;
 export function is(val, type) {
     return toString.call(val) === `[object ${type}]`;
 }
+export function isDef(val) {
+    return typeof val !== 'undefined';
+}
